74a6b83086df484077308999c06264d6b2b111b9
fikriwado <mfikri.khoirurrizal.com>
2025-07-08 13:23:41 +0700

    array

--- src/main.rs
+++ src/main.rs
@@ -207,3 +207,23 @@ fn unit_test ()
     let test: () = ();
     println!("{:?}", test);
 }
+
+#[test]
+fn array_test ()
+{
+    // let array: [i32; 5] = [1,2,3,4,5];
+
+    let mut array: [i32; 5] = [1,2,3,4,5];
+    println!("{:?}", array);
+
+    let a = array[0];
+    let b = array[1];
+    println!("{} {}", a, b);
+
+    array[0] = 10;
+    array[1] = 20;
+    println!("{:?}", array);
+
+    let length = array.len();
+    println!("{}", length);
+}
