417e6fe32d34000ed8fcbabb78556a5cffebc4f0
fikriwado <mfikri.khoirurrizal.com>
2025-07-08 10:37:33 +0700

    unit test, variable, static typing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn hello_test()
+{
+    println!("Hello Test");
+}
+
+#[test]
+fn variable_test()
+{
+    let name = "Moch Fikri Khoirurrizal";
+    println!("Hello {}", name);
+}
+
+#[test]
+fn mut_variable_test()
+{
+    let mut name = "Moch Fikri Khoirurrizal";
+    println!("Hello {}", name);
+
+    name = "Fikri Wado";
+    println!("Hello {}", name);
+}
+
+#[test]
+fn static_typing_test()
+{
+    // let mut name = "Moch Fikri Khoirurrizal";
+    let name = "Moch Fikri Khoirurrizal";
+    println!("Hello {}", name);
+
+    // name = 15;
+    println!("Hello {}", name);
+}
